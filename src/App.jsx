87f5313bfc7a68b4f2b1cca5488b63f36815b9bc
ItsPriya-Tech <priyaparmar1603@gmail.com>
2026-08-21 10:33:17 +0530

    Week 1: Initial React project setup

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+function App() {
+  return (
+    <div>
+      <h1>Student Expense Tracker</h1>
+
+      <p>Track your daily expenses easily.</p>
+
+      <h2>Total Expenses: ₹0</h2>
+
+      <button>Add Expense</button>
+    </div>
+  );
+}
+
+export default App;
